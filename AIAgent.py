8636433cd0bf70a0cfe3d0539c1b216cae41f179
Bacem Karray <99637476+bacemkarray@users.noreply.github.com>
2024-10-13 09:11:21 -0400

    Update userLocationAgent.py

--- AIAgent.py
+++ AIAgent.py
@@ -1,52 +1,56 @@
-"""
-This agent is capable of receiving messages and reply to the sender.
-"""
-
-from uagents import Agent, Context, Model
+# Imports
+from uagents import Model, Agent, Context
 import requests
 
-agent = Agent()
+
+# Create an agent
+agent = Agent(
+    name = "searchLocations",
+    seed = "khavaioghgjabougrvbosubvisgvgjfkf",
+    port = 8000,
+    endpoint = ["http://127.0.0.1:8000/submit"]
+)
+
 
 class Request(Model):
     message: str
 
-def get_attr(city_name, api):
+
+def get_attr(userLocationName, api):
     url = "https://maps.googleapis.com/maps/api/place/textsearch/json"
     params = {
-        'query': city_name,
+        'query': userLocationName,
         'key': api,
         'type': 'tourist_attraction',
     }
     response = requests.get(url, params)
     data = response.json()
-    li = []
-    # print(data)
-    # if 'results' in data:
+    locationList = []
+
     for place in data['results']:
         name = place.get('name')
         rating = place.get('rating')
-        addy = place.get('formatted_address')
-        li.append(f"{name}: {rating} stars\n{addy}\n")
-    # else:
-    #     return ("No results found.")
-    str2 = ''.join(li)
+        formAddress = place.get('formatted_address')
+        locationList.append(f"{name}: {rating} stars\n{formAddress}\n")
+    
+    str2 = ''.join(locationList)
     return str2
 
 
 
 @agent.on_message(model=Request)
 async def handle_message(ctx: Context, sender: str, msg: Request):
-    """Log the received message and reply to the sender"""
+    "Log the received message"
     city = msg.message
     api_key = 'AIzaSyBvo0dTu0VrP46PyTW8ORJuCllJxWxF3Wc'
     ctx.logger.info(f"Received message from {sender}: {msg.message}")
     ctx.logger.info(city)
     ctx.logger.info(get_attr(city, api_key))
     await ctx.send(sender, Request(message= get_attr(city, api_key)))
-    ctx.logger.info(f"Message has been sent to User Agent")
+    # ctx.logger.info(f"Message has been sent to User sendToLocator")
 
 
 
-
+    
 if __name__ == "__main__":
     agent.run()
